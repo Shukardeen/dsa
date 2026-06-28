64167ef5744a486c72d1f27db9a7d34a32d05b1f
Shukardeen <sonugahlot57016@gmail.com>
2026-06-28 17:02:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (53.83%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -11,26 +11,14 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode current = head;
-        int nodeCount = 0;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        // count the number of nodes
-        while(current != null) {
-            nodeCount++;
-            current = current.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        // middle node position --> (n/2)+1
-        int middleNode = nodeCount/2 + 1;
-        // reach the middle node and return
-        current = head;
-        while(current != null) {
-            middleNode--;
-            if(middleNode == 0) {
-                break;
-            }
-            current = current.next;
-        }
-        return current;
+        return slow;
     }
 }
