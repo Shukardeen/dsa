6a12b679b2a10fbd96ce28e60890a58592a04736
Shukardeen <sonugahlot57016@gmail.com>
2026-06-28 17:23:07 +0530

    Time: 10 ms (15.44%), Space: 98.1 MB (26.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,22 +11,43 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if(head.next == null) return true;
-        // stack to store data values
-        Stack<Integer> s = new Stack<>();
-        ListNode current = head;
+        // slow and fast pointers
+        ListNode slow = head;
+        ListNode fast = head;
 
-        // store values to stack
-        while(current != null) {
-            s.push(current.val);
-            current = current.next;
+        // step1: reach the middle node
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        // compare values
-        current = head;
-        while(current != null) {
-            if(current.val != s.pop()) return false;
-            current = current.next;
+        // step2: reverse the secondHalf
+        ListNode secondHalfHead = reverse(slow.next);
+
+        // step3: compare first and second half values
+        ListNode first = head;
+        ListNode second = secondHalfHead;
+        while(second != null) {
+            if(first.val != second.val) {
+                reverse(secondHalfHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;
         }
+        reverse(secondHalfHead);
         return true;
+    }
+
+    // method to reverse a linked list - recursive approach
+    static ListNode reverse(ListNode head) {
+        if(head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
     }
 }
