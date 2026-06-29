9d62f7ac7cc2ec38e41c6a6946b29841a6281007
Shukardeen <sonugahlot57016@gmail.com>
2026-06-29 13:51:57 +0530

    Time: 2 ms (51.22%), Space: 52.8 MB (62.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,42 +11,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // length variables
-        int lengthA = 0;
-        int lengthB = 0;
-
-        // compute the length of lists
         ListNode currentA = headA;
         ListNode currentB = headB;
-        while(currentA != null || currentB != null) {
-            if(currentA != null) {
-                lengthA++;
-                currentA = currentA.next;
-            }
-            if(currentB != null) {
-                lengthB++;
-                currentB = currentB.next;
-            }
-        }
 
-        currentA = headA;
-        currentB = headB;
-        if(lengthA < lengthB) return collisionPoint(currentA, currentB, lengthB-lengthA);
-        else return collisionPoint(currentB, currentA, lengthA-lengthB);
-    }
-
-    // function to find collision point
-    private static ListNode collisionPoint(ListNode smallerHead, ListNode largerHead, int steps) {
-        // move larger steps times
-        while(steps != 0) {
-            steps--;
-            largerHead = largerHead.next;
+        while(currentA != currentB) {
+            currentA = currentA.next;
+            currentB = currentB.next;
+            if(currentA == currentB) return currentA;
+            if(currentA == null) currentA = headB;
+            if(currentB == null) currentB = headA;
         }
-        // move both of them simultaneously
-        while(smallerHead != largerHead) {
-            smallerHead = smallerHead.next;
-            largerHead = largerHead.next;
-        }
-        return smallerHead;
+        return currentA;
     }
 }
