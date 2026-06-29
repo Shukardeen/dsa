d0224bb158659973bf81a84dff8bf9c839adb5d9
Shukardeen <sonugahlot57016@gmail.com>
2026-06-29 10:59:10 +0530

    Time: 6 ms (20.58%), Space: 48.7 MB (17.66%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -10,26 +10,7 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        // maintain carry
-        int carry = 0;
-        // reverse the linked list
-        head = reverse(head);
-
-        // traverse reversed linked list
-        ListNode current = head;
-        while(current != null) {
-            int val = current.val * 2 + carry;
-            if(val < 10) {
-                current.val = val;
-                carry = 0;
-            } else {
-                current.val =  val % 10;
-                carry = val / 10;
-            }
-            current = current.next;
-        }
-
-        head = reverse(head);
+        int carry = helper(head);
         if(carry != 0) {
             head = new ListNode(carry, head);
             return head;
@@ -37,12 +18,16 @@ class Solution {
         return head;
     }
 
-    static ListNode reverse(ListNode head) {
-        if(head.next == null) return head;
-        ListNode newHead = reverse(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
-        return newHead;
+    // recursive helper function
+    private int helper(ListNode head) {
+        if(head == null) return 0;
+        int carry = helper(head.next);
+        int val = head.val * 2 + carry;
+        if(val < 10) {
+            head.val = val;
+            return 0;
+        } 
+        head.val = val%10;
+        return val/10;
     }
 }
