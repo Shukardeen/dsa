71d34814e9e3ba95b9e98b1ddc4da173d165c85e
Shukardeen <sonugahlot57016@gmail.com>
2026-07-01 11:50:40 +0530

    Time: 5 ms (41.29%), Space: 46.6 MB (78.3%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,37 +10,45 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        if(lists.length == 1) return lists[0];
+        if (lists.length == 0)
+            return null;
+        if (lists.length == 1)
+            return lists[0];
 
-        ListNode head = lists[0];
-        for(int i=1; i<lists.length; i++) {
-            head = merge(head, lists[i]);
+        // min heap to store all heads
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.value, b.value));
+
+        // store all heads to priority queue
+        for(int i=0; i<lists.length; i++) {
+            ListNode head = lists[i];
+            if(head != null) {
+                pq.offer(new Pair(head.val, head));
+            }
         }
-        return head;
-    }
 
-    // method to merge two lists
-    private ListNode merge(ListNode head1, ListNode head2) {
-        if(head1 == null) return head2;
-        if(head2 == null) return head1;
-
-        ListNode current1 = head1;
-        ListNode current2 = head2;
+        // dummy head
         ListNode dummyHead = new ListNode(-1);
         ListNode dummyCurrent = dummyHead;
 
-        while(current1 != null && current2 != null) {
-            if(current1.val < current2.val) {
-                dummyCurrent.next = current1;
-                current1 = current1.next;
-            } else {
-                dummyCurrent.next = current2;
-                current2 = current2.next;
-            }
+        while(!pq.isEmpty()) {
+            Pair p = pq.poll();
+            dummyCurrent.next = p.node;
             dummyCurrent = dummyCurrent.next;
+            if(p.node.next != null) {
+                pq.offer(new Pair(p.node.next.val, p.node.next));
+            }
         }
-        dummyCurrent.next = (current1 == null) ? current2 : current1;
+
         return dummyHead.next;
+    }
+}
+
+class Pair {
+    int value;
+    ListNode node;
+
+    Pair(int value, ListNode node) {
+        this.value = value;
+        this.node = node;
     }
 }
