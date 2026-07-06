fee1c2e7ae7abb35f7d1cbeba7ebd9126d1e7316
Shukardeen <sonugahlot57016@gmail.com>
2026-07-06 11:02:17 +0530

    Time: 25 ms (23.18%), Space: 47.8 MB (73.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,13 +1,12 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         // find the max element in piles that is the maximum number of bananas that can be eaten per hour
-        int maxBananasPerHour = piles[0];
-        for(int i=1; i<piles.length; i++) {
-            if(piles[i] > maxBananasPerHour) maxBananasPerHour = piles[i];
+        int low = 1, high = piles[0];
+        for(int pile: piles) {
+            if(pile > high) high = pile;
         }
 
         // implement binary search to find the minimum bananas to be eaten per hour to eat all bananas in given hours
-        int low = 1, high = maxBananasPerHour;
         while(low<=high) {
             int mid = low+(high-low)/2;
             if(time(piles, mid) <= h) high = mid - 1;
@@ -18,8 +17,8 @@ class Solution {
     // method to find the total hours koko will take to eat all bananas when k number of bananas are eaten per hour
     private int time(int[] piles, int k) {
         int totalHours = 0;
-        for(int i=0; i<piles.length; i++) {
-            totalHours += Math.ceil((double) piles[i]/k);
+        for(int pile: piles) {
+            totalHours += Math.ceil((double) pile/k);
         }
         return totalHours;
     }
