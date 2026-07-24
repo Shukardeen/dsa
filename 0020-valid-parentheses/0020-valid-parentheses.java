7857fd6b4c642b541d5b45bf6a269ef4dfc2e958
Shukardeen <sonugahlot57016@gmail.com>
2026-07-24 10:01:45 +0530

    Time: 3 ms (86.57%), Space: 43.2 MB (73.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() == 1) return false;
+        // stack to store opening brackets
+        Deque<Character> st = new ArrayDeque<>();
+
+        // traverse the array
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            }
+            else {
+                if(st.isEmpty()) return false;
+                char topChar = st.pop();
+                if((topChar == '(' && ch == ')') || (topChar == '[' && ch == ']') || topChar == '{' && ch == '}') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
